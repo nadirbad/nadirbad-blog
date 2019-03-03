2ba721be0fe4df68e26838940d791fb69ee4d09b
Nadir Badnjevic <nadirbad@gmail.com>
2019-03-03 21:19:09 +0100

    Add first post page

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: "https://lumen.netlify.com",
   title: "Blog by Nadir Badnjevic",
   subtitle:
-    "A place where I share my experience with .NET, Web Development, Software Architectures, Tech leadership and more",
+    "A place where I share my experience with .NET, Web Development, Software Architectures, Tech leadership and more.",
   copyright: "© 2019 All rights reserved.",
   disqusShortname: "",
   postsPerPage: 4,
@@ -17,11 +17,11 @@ module.exports = {
     {
       label: "About me",
       path: "/pages/about"
-    },
-    {
+    }
+    /* {
       label: "Contact me",
       path: "/pages/contacts"
-    }
+    } */
   ],
   author: {
     name: "Nadir Badnjevic",
