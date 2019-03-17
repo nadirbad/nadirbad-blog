2f0821f4ffadb8ba58b0f7f3828f8ac51af0ac46
Nadir Badnjevic <nadirbad@gmail.com>
2019-03-17 21:16:18 +0100

    lint changes

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
 module.exports = {
-  url: "https://lumen.netlify.com",
-  title: "Blog by Nadir Badnjevic",
+  url: 'https://wwww.nadirbad.dev',
+  title: 'Blog by Nadir Badnjevic',
   subtitle:
-    "A place where I share my experience with .NET, Web Development, Software Architectures, Tech leadership and more.",
-  copyright: "© 2019 All rights reserved.",
-  disqusShortname: "",
+    'A place where I share my experience with .NET, Web Development, Software Architectures, Tech leadership and more.',
+  copyright: '© 2019 All rights reserved.',
+  disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: "UA-20398312-2",
+  googleAnalyticsId: 'UA-20398312-2',
   menu: [
     {
-      label: "Articles",
-      path: "/"
+      label: 'Articles',
+      path: '/'
     },
     {
-      label: "About me",
-      path: "/pages/about"
+      label: 'About me',
+      path: '/pages/about'
     }
     /* {
       label: "Contact me",
@@ -24,15 +24,15 @@ module.exports = {
     } */
   ],
   author: {
-    name: "Nadir Badnjevic",
-    photo: "/profile.jpg",
+    name: 'Nadir Badnjevic',
+    photo: '/profile.jpg',
     bio:
-    "A place where I share my experience with .NET, Web Development, Software Architectures, Tech leadership and more.",
+      'A place where I share my experience with .NET, Web Development, Software Architectures, Tech leadership and more.',
     contacts: {
-      email: "nadirbad@gmail.com",
-      linkedin: "nadir-badnjevic",
-      twitter: "nadirbad",
-      github: "nadirbad"
+      email: 'nadirbad@gmail.com',
+      linkedin: 'nadir-badnjevic',
+      twitter: 'nadirbad',
+      github: 'nadirbad'
     }
   }
 };
